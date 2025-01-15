546b1a123e2bbfa69887bc3f97a2fddd4aedd543
Hirotaro Matsumoto <matsumoto@hirotaro.net>
2025-01-15 09:22:30 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# website
-My profile website
+# Hirotaro Matsumoto's Website
+
+## Description
+
+This repo contains the codes used on my website.
+
+## Contact
+
+Hirotaro Matsumoto <[info[＠]hirotaro.net](mailto:info@hirotaro.net)>
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+function openBitcoinCard() {
+  let element = document.getElementById('bitcoin-card');
+  element.style.visibility = "visible";
+  return;
+}
+
+function closeBitcoinCard() {
+  let element = document.getElementById('bitcoin-card');
+  element.style.visibility = "hidden";
+  return;
+}
